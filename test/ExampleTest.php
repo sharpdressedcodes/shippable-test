e44fa673ffa804178db3108274974ed960a56f13
Greg <sharpdressedcodes@users.noreply.github.com>
2019-04-19 00:59:55 +1000

    Add assertion (#1)
    * Add assertion
    * Change shippable yaml

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -10,6 +10,7 @@ class ExampleTest extends TestCase {
 		$b = 2;
 
 		$this->assertEquals(3, $a + $b, 'Expected this to equal 3');
+		$this->assertEquals(5, 3 + 2, 'Expected this to equal 5');
 
 	}
 
